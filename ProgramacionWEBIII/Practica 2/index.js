91c135986df3de0af35d62cfdf6d25d5d5e22390
OscarCX9 <161672561+OscarCX9@users.noreply.github.com>
2025-11-11 11:47:30 -0400

    Add files via upload

---
+++ ProgramacionWEBIII/Practica 2/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import productoRutas from './rutas/productoRutas.js';
+const app = express();
+app.use(express.json());
+app.use('/', productoRutas);
+const puerto = 3001;
+app.listen(puerto, () => {
+  console.log(`Servidor en http://localhost:${puerto}`);
+});
